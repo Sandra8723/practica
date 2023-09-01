559c80db52e510dc174fabc28fa4234af43a44d8
Yuneidis <102307774+Yune08@users.noreply.github.com>
2023-09-01 15:10:23 -0500

    Se agg funcion de volver al menu

--- app.py
+++ app.py
@@ -1,63 +1,70 @@
 def ejercicio1():
-    numbers = [1,2,3,4,5,6,7,8,9,10,11,12]
+    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
     for item in reversed(numbers):
         print(item)
-    
-def ejercicio2():
-    tupla1=(4,5,6)
-    tupla2=(8,9,10)
-    suma=()
-    for i in range(len(tupla1)):
-     suma+=(tupla1[i]+tupla2[i],)    
-        
-    print(suma)
-   
-def ejercicio3():
-    conjuntoAex={1,2,3,4,5}
-    newconj=set()
-    for item in conjuntoAex:
-     newconj.add(item**2)
+    volver_al_menu()
 
+def ejercicio2():
+    tupla1 = (4, 5, 6)
+    tupla2 = (8, 9, 10)
+    suma = tuple(t1 + t2 for t1, t2 in zip(tupla1, tupla2))
+    print(suma)
+    volver_al_menu()
+
+def ejercicio3():
+    conjuntoAex = {1, 2, 3, 4, 5}
+    newconj = {item**2 for item in conjuntoAex}
     print(newconj)
-   
+    volver_al_menu()
 
 def ejercicio4():
-    nombre=input('¿como te llamas?')
-    edad=input('¿cuantos años tienes')
+    nombre = input('¿Como te llamas? ')
+    edad = input('¿Cuantos años tienes? ')
+    print(f"Hola {nombre}, tu edad es {edad}")
+    volver_al_menu()
 
-    print("Hola "+nombre+" tu edad es "+edad)
 def ejercicio5():
-    nota1=float(input('ingresar nota 1 '))
-    nota2=float(input('ingresar nota 2 '))
-    nota3=float(input('ingresar nota 3 '))
+    nota1 = float(input('Ingresar nota 1: '))
+    nota2 = float(input('Ingresar nota 2: '))
+    nota3 = float(input('Ingresar nota 3: '))
+    promedio = (nota1 + nota2 + nota3) / 3
+    print(f"El promedio de las notas es: {promedio}")
+    volver_al_menu()
 
-    print(((nota1+nota2+nota3)/3))
-    
+def volver_al_menu():
+    respuesta = input("¿Deseas volver al menú principal? (1 para sí, 2 para no): ")
+    if respuesta == '1':
+        menu_principal()
+    elif respuesta == '2':
+        print("Menu de ejercicios finalizado")
+        exit()
+    else:
+        print("Respuesta no valida, seleccione 1 para volver al menú o 2 para salir del programa.")
 
-print("Menu de ejercicios")
-print("Seleccione una opcion: (1-5)")
-print("1. ejercicio 1")
-print("2. ejercicio 2")
-print("3. ejercicio 3")
-print("4. ejercicio 4")
-print("5. ejercicio 5")
+def menu_principal():
+    print("\nMenu de ejercicios")
+    print("Seleccione una opcion: (1-5)")
+    print("1. ejercicio 1")
+    print("2. ejercicio 2")
+    print("3. ejercicio 3")
+    print("4. ejercicio 4")
+    print("5. ejercicio 5")
 
-opcion= int(input("Ingrese el numero deseado: "))
+    opcion = int(input("Ingrese el numero deseado: "))
 
-if opcion==1:
-    ejercicio1()
-elif opcion==2:
-    ejercicio2()
-elif opcion==3:
-    ejercicio3()
-elif opcion==4:
-    ejercicio4()
-elif opcion==5:
-    ejercicio5()
+    if opcion == 1:
+        ejercicio1()
+    elif opcion == 2:
+        ejercicio2()
+    elif opcion == 3:
+        ejercicio3()
+    elif opcion == 4:
+        ejercicio4()
+    elif opcion == 5:
+        ejercicio5()
+    else:
+        print("Opción no válida. Por favor, seleccione una opción del menú (1-5).")
 
-##ejercicio 1: almacenar una lista de numeros del 1 al 12 y los muestre por pantalla en orden inverso
-##ejercicio 2: crear una sumatoria de dos listas
-##ejercicio 3: creacion de un nuevo conjunto basado en el primer conjunto, cuyo valores sean al cuadrado
-##ejercicio 4: pedir un nombre y una edad y traiga un mensaje de hola persona y la edad
-##ejercicio 5:promedio de notas
+menu_principal()
+
 
